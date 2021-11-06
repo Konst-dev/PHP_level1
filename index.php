808c265a4c8725f271e5480ddf08d12adb44d43d
konst <developerkae@outlook.com>
2021-11-06 20:13:14 +0300

    Initial project version

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$h1="Мой сайт на PHP";
+$title="Проверка PHP";
+$year=date('Y');
+?>
+<HTML>
+    <HEAD>
+        <TITLE>
+            <?php echo $title ?>
+        </TITLE>
+    </HEAD>
+    <BODY>
+        <h1><?php echo $h1 ?></h1>
+        <h2><?php echo $year ?></h2>
+
+    </BODY>
+</HTML>    
