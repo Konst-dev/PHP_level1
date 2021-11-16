e12a08a25e92a60a3745bf82481fd3d9ed7ed3fd
Konst-dev <developerkae@outlook.com>
2021-11-16 18:37:32 +0300

    final Урок 4

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -7,7 +7,7 @@ window.onload = function () {
     for (var i = 0; i < cards.length; i++) {
         cards[i].addEventListener('click', (e) => {
             var urls = e.currentTarget.getAttribute('src');
-            // window.open(urls, "_blank"); 
+            // window.open(urls, "_blank"); //Открытие картинки в новой вкладке
             modal_window.setAttribute('class', 'modal_show');
             var vSize = Math.trunc(document.documentElement.clientHeight * 0.95);
             modal_window.innerHTML = "<center><img class='big_Img'  src='" + urls + "' height='" + vSize + "px'></center>";
